ea74c5d86a40ad0950b2f4e256a6487dd1f8a2d2
John Newcomb <jnewcs@stanford.edu>
2015-03-20 14:05:59 -0700

    Environment variable changes

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module Dnewcomb
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Allows environment variables to be set before anything else
+    config.before_configuration do
+    env_file = File.join(Rails.root, 'config', 'local_env.yml')
+        YAML.load(File.open(env_file)).each do |key, value|
+            ENV[key.to_s] = value
+        end if File.exists?(env_file)
+    end
+      
   end
 end
