aac91354e1f5b89147cd455aa1a5e46f53efe07f
John Newcomb <jnewcs@stanford.edu>
2015-03-25 14:00:24 -0700

    Mobile optimizations

--- app/assets/javascripts/dnewcomb.js
+++ app/assets/javascripts/dnewcomb.js
@@ -59,6 +59,8 @@ $(function() {
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+$(document).ready(function() {
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
 });
