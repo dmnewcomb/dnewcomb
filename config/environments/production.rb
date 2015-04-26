bf73d56674f17e06174ebb7860ce0ec049e4ff1b
John Newcomb <jnewcs@stanford.edu>
2015-04-26 10:59:53 -0700

    Precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,6 @@ Rails.application.configure do
     
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = true
-  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
